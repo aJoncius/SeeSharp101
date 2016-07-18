0af404218ba04df6d9a4ad3d60be5e807bdd7e1b
aJoncius <aurimas.joncius@gmail.com>
2016-07-18 14:06:29 +0300

    Add project files.

---
+++ SeeSharp101.Tests/Basics/GraphTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeeSharp101.Basics;
+
+namespace SeeSharp101.tests.Basics
+{
+    /// <summary>
+    /// Summary description for GraphTests
+    /// </summary>
+    [TestClass]
+    public class GraphTests
+    {
+        public GraphTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Should_Return_Correct_Identity()
+        {
+            var graph = new Graph();
+            Assert.AreEqual(graph.GetName(), "Master Graph");
+            Assert.AreEqual(graph.GetDescription(), "One Graph to Rule Them All");
+        }
+    }
+}
---
+++ SeeSharp101/Basics/Graph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeeSharp101.Basics
+{
+    public class Graph
+    {
+        private string _name = "Master Graph";
+        private string _description = "One Graph to Rule Them All";
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string GetDescription()
+        {
+            return _description;
+        }
+    }
+}
